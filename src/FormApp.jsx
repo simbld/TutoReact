84952002d77b48a09136fea12195e223a819a8d2
sim <s_bullado51@hotmail.com>
2023-11-13 06:59:10 +0100

    création d'un bouton 'reset' avec champ controllé par handleChange

--- src/FormApp.jsx
+++ src/FormApp.jsx
@@ -5,10 +5,14 @@ function FormApp() {
     const handleChange = (e) => {
         setFirstname(e.target.value)
     }
+    const reset = () => {
+    setFirstname('')
+    }
 
     return <form>
         <input type="text" name="firstname" value={firstname} onChange={handleChange}/>
         {firstname}
+    <button onClick={reset} type="button">Reset</button>
     </form>
 }
 
